e2dd1cbb80cf9c38aa78d647dcb792955a83817a
jeanfrancoisvo <jeanfrancoisvo7@gmail.com>
2022-12-05 13:24:08 +0100

    Day1: Part1

---
+++ src/main/kotlin/day1/Day1.kt
@@ -0,0 +1,18 @@
+package day1
+
+import java.io.File
+
+fun main() {
+    val lines = File("src/main/resources/day1.txt").readLines()
+    val grouped = lines.fold(mutableListOf<MutableList<String>>(mutableListOf())) { acc, s ->
+        if (s.isBlank()) {
+            acc.add(mutableListOf())
+            acc
+        } else {
+            acc.last().add(s)
+            acc
+        }
+    }
+    println("Max top 1 : ${grouped.maxOfOrNull { list -> list.sumOf { it.toInt() } }}")
+
+}
