5b8f52b07b3bee0f8629ce7a6b3c9f60c750a75c
jeanfrancoisvo <jeanfrancoisvo7@gmail.com>
2022-12-07 09:47:43 +0100

    Day2: Part1

--- src/main/kotlin/Day2.kt
+++ src/main/kotlin/Day2.kt
@@ -1,17 +1,55 @@
 import java.io.File
 
-fun day2() {
-    val lines = File("src/main/resources/day1.txt").readLines()
-    val grouped = lines.fold(mutableListOf<MutableList<String>>(mutableListOf())) { acc, s ->
-        if (s.isBlank()) {
-            acc.add(mutableListOf())
-            acc
-        } else {
-            acc.last().add(s)
-            acc
-        }
+enum class Victor(val score: Int) {
+    ME(6),
+    OPPONENT(0),
+    DRAW(3)
+}
+
+enum class Move(val score: Int) {
+    ROCK(1) {
+        override fun winAgainst(move: Move) = move == SCISSOR
+    },
+    PAPER(2) {
+        override fun winAgainst(move: Move) = move == ROCK
+    },
+    SCISSOR(3) {
+        override fun winAgainst(move: Move) = move == PAPER
+    };
+
+    companion object {
+        fun mine(my: String): Move =
+            when (my) {
+                "X" -> ROCK
+                "Y" -> PAPER
+                else -> SCISSOR
+            }
+
+        fun opponent(opponent: String): Move =
+            when (opponent) {
+                "A" -> ROCK
+                "B" -> PAPER
+                else -> SCISSOR
+            }
     }
-    println("Max top 1 : ${grouped.maxOfOrNull { list -> list.sumOf { it.toInt() } }}")
-    println("Sum top 3 : ${grouped.map { list -> list.sumOf { it.toInt() } }.sortedDescending().take(3).sum()}")
+
+    abstract fun winAgainst(move: Move): Boolean
+}
+
+fun day2() {
+    val lines = File("src/main/resources/day2.txt").readLines()
+    val result = lines
+        .map { it.split(" ") }
+        .map { Pair(Move.opponent(it[0]), Move.mine(it[1])) }
+        .map {
+            if (it.first == it.second) {
+                Pair(Victor.DRAW, it.second)
+            } else if (it.first.winAgainst(it.second)) {
+                Pair(Victor.OPPONENT, it.second)
+            } else {
+                Pair(Victor.ME, it.second)
+            }
+        }
+    println("Total : ${result.sumOf { it.first.score + it.second.score }}")
 
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,3 @@
-fun main(args: Array<String>) {
-    day1()
+fun main() {
+    day2()
 }
