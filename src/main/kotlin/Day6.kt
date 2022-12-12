a5d3ffceeafacd654371c9074e072ab497e268aa
jeanfrancoisvo <jeanfrancoisvo7@gmail.com>
2022-12-12 16:46:48 +0100

    Some refacto

--- src/main/kotlin/Day6.kt
+++ src/main/kotlin/Day6.kt
@@ -1,20 +1,7 @@
 fun main() {
-    val lines = readInput("day6")
-    val line = lines[0]
-    day6(line, 4) // 1109
-    day6(line, 14) // 3965
+    val line = readInput("day6").first()
+    println(day6(line, 4)) // 1109
+    println(day6(line, 14)) // 3965
 }
 
-private fun day6(input: String, window: Int) {
-    var i = 0
-    var found = false
-    while (i < input.length && !found) {
-        found = !input.drop(i++).take(window).haveDuplicate()
-    }
-    println(i + window - 1)
-}
-
-private fun String.haveDuplicate(): Boolean {
-    return this.groupingBy { it }.eachCount()
-        .count { it.value > 1 } > 0
-}
+private fun day6(input: String, size: Int): Int = input.windowed(size).indexOfFirst { it.toSet().size == size } + size
