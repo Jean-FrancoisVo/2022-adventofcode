69b3d136e247e6045a290fb9986bc26048e8b24b
jeanfrancoisvo <jeanfrancoisvo7@gmail.com>
2022-12-15 11:28:07 +0100

    Day8: Part2

--- src/main/kotlin/Day08.kt
+++ src/main/kotlin/Day08.kt
@@ -10,14 +10,14 @@ fun day8(lines: List<String>) {
             .map { row -> (1 until matrix[0].size - 1).zip(Array(matrix[0].size - 1) { row }) }
             .flatten()
             .count { isVisible(matrix, it) } + matrix.size * 2 + matrix[0].size * 2 - 4
-//    val part2 =
-//        (1 until matrix.size - 1)
-//            .map { row -> (1 until matrix[0].size - 1).zip(Array(matrix[0].size - 1) { row }) }
-//            .flatten().maxOfOrNull { isVisible2(matrix, it) }
-//    println(part1)
-//    println(part1 == 1827)
-    println(isVisible2(matrix, 3 to 2))
-//    println(part2)
+    val part2 =
+        (1 until matrix.size - 1)
+            .map { row -> (1 until matrix[0].size - 1).zip(Array(matrix[0].size - 1) { row }) }
+            .flatten().maxOfOrNull { isVisible2(matrix, it) }
+    println(part1)
+    println(part1 == 1827)
+    println(part2)
+    println(part2 == 335580)
 }
 
 private fun isVisible(matrix: List<List<Int>>, position: Pair<Int, Int>): Boolean {
@@ -34,21 +34,24 @@ private fun isVisible2(matrix: List<List<Int>>, position: Pair<Int, Int>): Int {
     val (row, column) = position
     val rows = matrix.rowsAt(row)
     val columns = matrix.columnsAt(column)
-    println(rows.toto((column - 1 downTo 0), column))
-    println(rows.toto((column + 1 until columns.size), column))
-    println(columns.toto((row - 1 downTo 0), row))
-    println(columns.toto((row + 1 until rows.size), row))
-    return rows.toto((column - 1 downTo  0), column) * // LEFT
-            rows.toto((column + 1 until columns.size), column) * // RIGHT
-            columns.toto((row - 1 downTo  0), row) * // TOP
-            columns.toto((row + 1 until rows.size), row) // BOTTOM
+    return rows.toto((column - 1 downTo  0), column) *
+            rows.toto((column + 1 until columns.size), column) *
+            columns.toto((row - 1 downTo  0), row) *
+            columns.toto((row + 1 until rows.size), row)
 }
 
 private fun List<Int>.isVisibleWithin(range: IntProgression, position: Int) =
     this.filterIndexed { index, i -> index in range && i >= this[position] }
         .isEmpty()
-private fun List<Int>.toto(range: IntProgression, position: Int) =
-    range.map{ this[it] }.takeWhile { it < this[position] }
-        .count()
+
+private fun List<Int>.toto(range: IntProgression, position: Int): Int {
+    val biggerTree = range.map { this[it] }.indexOfFirst { it >= this[position] } + 1
+    return if (biggerTree == 0) {
+        range.count()
+    } else {
+        range.map{ this[it] }.take(biggerTree).count()
+    }
+}
+
 private fun <E> List<List<E>>.rowsAt(x: Int): List<E> = this[x]
 private fun <E> List<List<E>>.columnsAt(y: Int): List<E> = this.transpose2d()[y]
