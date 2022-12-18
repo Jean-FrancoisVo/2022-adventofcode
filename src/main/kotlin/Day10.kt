99d6ef0514946e86acfcc3949cb2472a88ceb8f5
jeanfrancoisvo <jeanfrancoisvo7@gmail.com>
2022-12-18 10:44:27 +0100

    Day10: Part2

--- src/main/kotlin/Day10.kt
+++ src/main/kotlin/Day10.kt
@@ -2,9 +2,38 @@ fun main() {
     val lines = readInput("day10")
     println(day10Part1(lines))
     println(day10Part1(lines) == 14320)
+    println(day10Part2(lines).joinToString(" \n"))
 }
 
 fun day10Part1(lines: List<String>): Int {
+    val states = cpuStates(lines)
+    println(states)
+    return 20 * states.find { it.first == 20 }!!.second +
+            60 * states.find { it.first == 60 }!!.second +
+            100 * states.find { it.first == 100 }!!.second +
+            140 * states.find { it.first == 140 }!!.second +
+            180 * states.find { it.first == 180 }!!.second +
+            220 * states.find { it.first == 220 }!!.second
+}
+
+fun day10Part2(lines: List<String>): MutableList<String> {
+    val crtLines = MutableList(8) { "" }
+    var crtIndex = 0
+    cpuStates(lines).forEach { state ->
+        val (cycle, x) = state
+        crtLines[crtIndex] += if (crtLines[crtIndex].length % 40 in x - 1..x + 1) {
+            "#"
+        } else {
+            "."
+        }
+        if (cycle % 40 == 0) {
+            crtIndex++
+        }
+    }
+    return crtLines
+}
+
+private fun cpuStates(lines: List<String>): List<Pair<Int, Int>> {
     val instructions = lines.fold(mutableListOf(1 to 1)) { acc, line ->
         when (line.take(4)) {
             "addx" -> {
@@ -21,10 +50,5 @@ fun day10Part1(lines: List<String>): Int {
             else -> error("")
         }
     }
-    return 20 * instructions.find { it.first == 20 }!!.second +
-            60 * instructions.find { it.first == 60 }!!.second +
-            100 * instructions.find { it.first == 100 }!!.second +
-            140 * instructions.find { it.first == 140 }!!.second +
-            180 * instructions.find { it.first == 180 }!!.second +
-            220 * instructions.find { it.first == 220 }!!.second
+    return instructions.dropLast(1)
 }
