4a4c43dc8334763c254e0580a2a3711739959d8c
jeanfrancoisvo <jeanfrancoisvo7@gmail.com>
2022-12-21 18:37:40 +0100

    Day11: Part2

--- src/main/kotlin/Day11.kt
+++ src/main/kotlin/Day11.kt
@@ -1,18 +1,14 @@
 fun main() {
     val lines = readInput("day11")
-    println(day11Part1(lines))
+    println(day11Part1(lines) == 119715L)
+    println(day11Part2(lines) == 18085004878L)
 }
 
-fun day11Part1(lines: List<String>): Int {
+fun day11Part1(lines: List<String>): Long {
     val monkeys = lines.chunked(7)
         .map { config ->
-            val items = """(\d+)""".toRegex().findAll(config[1])
-                .map { it.groupValues }.map { it.first().toInt() }
-                .toMutableList()
-            val operand = config[2].split(" ").last()
-            val operator: (Int, Int) -> Int = if (config[2].contains("*")) Int::times else Int::plus
-            val operation = { v: Int -> operator(v, if (operand == "old") v else operand.toInt()) }
-            Monkey(items, operation, Triple(config[3].lastInt(), config[4].lastInt(), config[5].lastInt()))
+            val (items, operation) = itemsAndOperation(config)
+            Monkey(items, operation, config[3].lastLong(), config[4].lastInt(), config[5].lastInt()) { a -> a / 3 }
         }
     repeat(20) {
         monkeys.forEach { monkey ->
@@ -22,34 +18,72 @@ fun day11Part1(lines: List<String>): Int {
     return monkeys.map { it.inspected }.sortedDescending().take(2).fold(1) { acc, i -> acc * i }
 }
 
+
+fun day11Part2(lines: List<String>): Long {
+    val leastCommonMultiple = lines.chunked(7).map { it[3].lastInt().toLong() }.fold(1L) { acc, i -> acc * i }
+    val monkeys = lines.chunked(7)
+        .map { config ->
+            val (items, operation) = itemsAndOperation(config)
+            Monkey(items, operation, config[3].lastLong(), config[4].lastInt(), config[5].lastInt()) { a -> a % leastCommonMultiple}
+        }
+    repeat(10_000) {
+        monkeys.forEach { monkey ->
+            monkey.inspect().forEach { monkeys[it.first].items.add(it.second) }
+        }
+    }
+    return monkeys.map { it.inspected }.sortedDescending().take(2).fold(1L) { acc, i -> acc * i }
+}
+
+private fun itemsAndOperation(config: List<String>): Pair<MutableList<Long>, (Long) -> Long> {
+    val items = """(\d+)""".toRegex().findAll(config[1])
+        .map { it.groupValues }.map { it.first().toLong() }
+        .toMutableList()
+    val operand = config[2].split(" ").last()
+    val operator: (Long, Long) -> Long = if (config[2].contains("*")) Long::times else Long::plus
+    val operation = { v: Long -> operator(v, if (operand == "old") v else operand.toLong()) }
+    return Pair(items, operation)
+}
+
+typealias DivideBy = Long
+typealias TrueMonkey = Int
+typealias FalseMonkey = Int
+typealias MonkeyIndex = Int
+
 class Monkey(
-    val items: MutableList<Int>,
-    val operation: (v: Int) -> Int,
-    private val test: Triple<Int, Int, Int>
+    val items: MutableList<Long>,
+    val operation: (v: Long) -> Long,
+    private val test: DivideBy,
+    private val trueMonkey: TrueMonkey,
+    private val falseMonkey: FalseMonkey,
+    private val loweringWorryLevel: (Long) -> Long
 ) {
     var inspected = 0
 
-    fun inspect(): List<Pair<Int, Int>> {
-        val result = items.map { operation(it) }
+    fun inspect(): List<Pair<MonkeyIndex, Long>> {
+        val result = items
+            .map { loweringWorryLevel(operation(it)) }
             .map {
-                val worry = (it / 3)
-                if (worry % test.first == 0) {
-                    test.second to worry
+                if (it divisibleBy test) {
+                    trueMonkey to it
                 } else {
-                    test.third to worry
+                    falseMonkey to it
                 }
             }
         inspected += result.size
         items.empty()
         return result
     }
+
     override fun toString(): String {
-        return "Monkey(items=$items,inspected=$inspected)"
+        return "Monkey(inspected=$inspected)"
     }
 }
+
+private infix fun Long.divisibleBy(value: Long): Boolean = this % value == 0L
 
 private fun <E> MutableList<E>.empty() {
     repeat(this.size) { this.removeFirst() }
 }
 
 private fun String.lastInt(): Int = this.split(" ").last().toInt()
+private fun String.lastLong(): Long = this.split(" ").last().toLong()
